cf9c109fa2be985b012b512a325efecec5b099a3
Agus Sepriyandiansah <55836296+AgusS705@users.noreply.github.com>
2020-10-28 16:51:50 +0700

    Add files via upload

---
+++ lib/ui/screens/reset.dart
@@ -0,0 +1,134 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ResetPage extends StatefulWidget {
+  @override
+  ResetPageState createState() => ResetPageState();
+}
+
+class ResetPageState extends State<ResetPage> {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/bg.PNG'),
+                    fit: BoxFit.cover)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 50),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Lupa', style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('password', style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Kami akan mengirim link reset', style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('password ke email anda silahkan', style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('masukkan email yang terhubung ', style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('dengan akun :', style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 50),
+                      ),
+                      Flexible(
+                        child: TextField(
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            labelStyle: TextStyle(color: Colors.white),
+                            hintText: 'your username@gmail.com',
+                            hintStyle: TextStyle(color: Colors.white),
+                            enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                            focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                            border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 50),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 40),
+                ),
+                Container(
+                  width: screenSize.width * 0.6,
+                  height: screenSize.width * 0.12,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: FlatButton(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      color: Colors.white,
+                      onPressed: () {},
+                      child: Text('Reset password', style: TextStyle(color: Color(0xFFFBC02D),fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+      ),
+    );
+  }
+}
---
+++ lib/ui/screens/signup.dart
@@ -0,0 +1,180 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:login/signupberhasil.dart';
+
+class SignupPage extends StatefulWidget {
+  @override
+  SignupPageState createState() => SignupPageState();
+}
+
+class SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/bg.PNG'),
+                    fit: BoxFit.cover)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 150),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Daftar akun', style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Smarthome', style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 50),
+                      ),
+                      Flexible(
+                        child: TextField(
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            labelText: 'Nama',
+                            labelStyle: TextStyle(color: Colors.white),
+                            hintText: 'Dummy Name',
+                            hintStyle: TextStyle(color: Colors.white),
+                            enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                            focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                            border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 50),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 50),
+                      ),
+                      Flexible(
+                        child: TextField(
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            labelStyle: TextStyle(color: Colors.white),
+                            hintText: 'your username@gmail.com',
+                            hintStyle: TextStyle(color: Colors.white),
+                            enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                            focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                            border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 50),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(padding: EdgeInsets.only(left: 50),),
+                      Flexible(
+                        child: TextField(
+                          obscureText: true,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(color: Colors.white),
+                            hintText: 'Enter your password',
+                            hintStyle: TextStyle(color: Colors.white),
+                            enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                            focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                            border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 50),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(padding: EdgeInsets.only(left: 50),),
+                      Flexible(
+                        child: TextField(
+                          obscureText: true,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            labelText: 'Konfirmasi Password',
+                            labelStyle: TextStyle(color: Colors.white),
+                            hintText: 'Enter your password',
+                            hintStyle: TextStyle(color: Colors.white),
+                            enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                            focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                            border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 50),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                ),
+                Container(
+                  width: screenSize.width * 0.6,
+                  height: screenSize.width * 0.12,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: FlatButton(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignupBerhasil()),
+                        );
+                      },
+                      child: Text('Buat Akun >', style: TextStyle(color: Color(0xFFFBC02D),fontSize: 25, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+      ),
+    );
+  }
+}
