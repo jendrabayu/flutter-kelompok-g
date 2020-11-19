eea5743206619e36d99e2383e7f9c1de4756ba00
mfathonyramdhan <mfathonyramdhan@gmail.com>
2020-11-19 23:19:49 +0700

    Added splashScreen v1

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) {
+          return HomePage();
+        }),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffFFCC24),
+      body: Center(
+
+        child: Image.asset(
+            "assets/images/icon-logo.png"
+        ),
+      ),
+    );
+  }
+}
